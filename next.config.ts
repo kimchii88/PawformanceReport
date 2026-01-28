3695e0d227b7ef1529670cd355c4308d21a50668
kimchi8 <kimmygao808@gmail.com>
2026-01-28 22:33:31 +0000

    change config params

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    images: {
-        unoptimized: true,
-    },
+    images: { unoptimized: true },
+    basePath: "/PawformanceReport",
+    assetPrefix: "/PawformanceReport/",
+    trailingSlash: true,
 };
+
 module.exports = nextConfig;
